cb69b07c21405f029c2f293c0548df1f1628c554
Erik Dillingham <erik.dillingham@gmail.com>
2018-03-23 09:01:03 -0700

    fixed typos

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,10 @@
-// write a simple function to determine if a string is a palidrome
+// write a simple function to determine if a string is a palindrome
 
 console.log(isPalindrome("level"));                   // should return true
 console.log(isPalindrome("levels"));                  // should return false
 console.log(isPalindrome("A car, a man, a maraca"));  // should return true
 
-// bonus: why is it valid to declare this function *afte*r calls to it in source code?
+// bonus: why is it valid to declare this function *after* calls to it in source code?
 function isPalindrome(str) {
   // your code here
 }
